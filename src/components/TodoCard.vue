af915cd1afae3648fb00e666fe031e6df3a00b97
Deyvisson Augusto <deyvisson@stc.ma.gov.br>
2020-03-06 15:46:38 -0300

    adicionando componente para inserção de dados

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -2,22 +2,43 @@
     <div class="card">
         <header class="card-header">
             <p class="card-header-title has-text-left">
-                Dia
+                {{hoje}}
             </p>
             <div class="has-text-right">
-                <p class="card-header-title">0 Tarefas</p>
+                <p class="card-header-title">{{tarefas.length}} tarefas</p>
             </div>
         </header>
         <div class="card-content">
-            <div class="content"></div>
+            <div class="content">
+                <novo-todo></novo-todo>
+            </div>
             <div class="content"></div>
         </div>
     </div>
 </template>
 
 <script>
+
+import NovoTodo from './NovoTodo'
+
 export default {
-    name: 'todo-card'
+    name: 'todo-card',
+    components: {
+        NovoTodo
+    },
+    data(){
+        return {
+            dias: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+            meses: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            tarefas: []
+        }
+    },
+    computed: {
+        hoje: function(){
+            let novaData = new Date()
+            return `${this.dias[novaData.getDay()]}, ${novaData.getDate()} de ${this.meses[novaData.getMonth()]}`
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import TodoCard from './components/TodoCard.vue'
+import NovoTodo from './components/NovoTodo.vue'
 
 new Vue({
     el: '#app',
